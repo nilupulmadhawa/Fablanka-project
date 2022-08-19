9d7b5a05f9d9bde3db63881114712410a3951f8b
Samitha99 <73548027+Samitha99@users.noreply.github.com>
2022-08-19 19:31:58 +0530

    Update index.js

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -63,49 +63,52 @@ const homePage = () => {
       title="FabLanka | Home"
       content="Home page for this auth tutorial on httpOnly cookies with json web tokens"
     >
-      <div className="container-fuild">
-        <div data-aos="fade-right" className="row">
-          <div className="col-lg-8 rounded-3">
-            <ImageSlider />
-          </div>
-          <div data-aos="fade-left" className="col-lg-4 d-sm-block">
-            <div>
-              <NewAlert />
+      <div className="container-fluid">
+        <div className="relative">
+          <div data-aos="fade-right" className="row">
+            <div className="col-lg-8 rounded-3">
+              <ImageSlider />
             </div>
           </div>
-        </div>
-        <div className="row">
-          <p className="text-justify col-lg-12 mt-3">
-            Fab Lanka is a group of highly motivated individuals coming together
-            to set up fabrication labs (fab labs) throughout Sri Lanka as a
-            community outreach project. The abundance of an educated work force,
-            high degree of IT usage, and existence of a large number of small
-            and medium size manufactures (SMEs) makes Sri Lanka an ideal home
-            for fab labs. The benefits of bringing in new technology is the
-            ability to transform the manufacturing sector of local economies in
-            environmentally friendly ways. In addition, it will also strengthen
-            employment for youth in the 21st century manufacturing job sector.
-          </p>
-
-          <div className="text-justify col-lg-12 mt-3 mb-3">
-            <p>
-              Inspired by the technologically based social movements such as
-              Open Source Ecology, FabLab, and RepRap who’s aim is to spread
-              technology and knowledge to build a more equitable society,
-              FabLanka aspires to provide a platform for social and economic
-              development through education and technological innovation.
+          <div className="row">
+            <p className="text-justify col-lg-12 mt-3">
+              Fab Lanka is a group of highly motivated individuals coming
+              together to set up fabrication labs (fab labs) throughout Sri
+              Lanka as a community outreach project. The abundance of an
+              educated work force, high degree of IT usage, and existence of a
+              large number of small and medium size manufactures (SMEs) makes
+              Sri Lanka an ideal home for fab labs. The benefits of bringing in
+              new technology is the ability to transform the manufacturing
+              sector of local economies in environmentally friendly ways. In
+              addition, it will also strengthen employment for youth in the 21st
+              century manufacturing job sector.
             </p>
-            {readMore && extraContent}
-            <a
-              className="read-more-link mt-3"
-              onClick={() => {
-                setReadMore(!readMore);
-              }}
-            >
-              <p className=" text-sm mt-3">
-                <samll>{linkName}</samll>
+            <div className="text-justify col-lg-12 mt-3 mb-3">
+              <p>
+                Inspired by the technologically based social movements such as
+                Open Source Ecology, FabLab, and RepRap who’s aim is to spread
+                technology and knowledge to build a more equitable society,
+                FabLanka aspires to provide a platform for social and economic
+                development through education and technological innovation.
               </p>
-            </a>
+              {readMore && extraContent}
+              <a
+                className="read-more-link mt-3"
+                onClick={() => {
+                  setReadMore(!readMore);
+                }}
+              >
+                <p className=" text-sm mt-3">
+                  <samll>{linkName}</samll>
+                </p>
+              </a>
+            </div>
+            <div
+              data-aos="fade-left"
+              className="col-lg-4 lg:absolute top-0 right-0 lg:top-0 right-0"
+            >
+              <NewAlert />
+            </div>
           </div>
         </div>
         <div className="row">
