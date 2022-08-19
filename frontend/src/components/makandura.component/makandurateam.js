963973234c601457036d78a140861ea37fd44677
theekshanaj123 <106722853+theekshanaj123@users.noreply.github.com>
2022-08-19 10:44:06 +0530

    1

---
+++ frontend/src/components/makandura.component/makandurateam.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../../hocs/Layout";
+import styles from "./makandura.module.css";
+
+
+const MakanduraTeam = () => {
+    return(
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+
+export default MakanduraTeam;
--- frontend/src/pages/education.js
+++ frontend/src/pages/education.js
@@ -4,8 +4,50 @@ import Layout from "../hocs/Layout";
 const education = () => {
   return (
     <Layout>
-      <div>
-        <h1>Education Page</h1>
+      <div className="container-fluid" >
+        <div> 
+          <p className="mt-10">
+          FabLanka aims to deliver free and structured courses for community education and commercial purposes. The courses will include training on CAD software, use and troubleshooting of fabrication machinery, and business development. An internationally recognized certificate program will be offered for motivated individuals to learn new skills that will help them in the job market. In addition, there will be unstructured programs such as tutorials, trainings, and workshops on a need basis. With partnerships between local colleges and businesses, FabLanka can initiate local economic development projects. FabLanka will serve as a resource hub for local communities, schools, and universities to access knowledge and development of technology.
+          </p>
+
+          <h5 className="mt-10">
+          Currently, FabLanka is offering a 6 months long Education program. The objectives of this Fab Education program are:
+          </h5>
+
+          <ul>
+            <li className="mt-3">Dispersion of advanced technologies to rural communities</li>
+            <li className="mt-3">Tap the potential of untapped human workforce and motivate them to believe and innovate by providing access to advanced technologies</li>
+            <li className="mt-3 mb-5">Develop core capabilities to product development and management</li>
+          </ul>
+
+          <h5>
+          The participants who will complete this 6 month Fab Education program will gain expertise in the following areas:
+          </h5>
+
+          <ul>
+            <li className="mt-3"> 3D Modeling</li>
+            <li className="mt-3">3D Printing</li>
+            <li className="mt-3">CNC Programming and machining</li>
+            <li className="mt-3">Laser Cutting/Engraving</li>
+            <li className="mt-3">Creating Arts and Crafts using software and digitally controlled machines</li>
+            <li className="mt-3 mb-5">Building and programming automated control devices using Arduino controller</li>
+          </ul>
+
+          <h5>
+            FabLanka Philosophy in conducting the training program is:
+          </h5>
+
+          <ul>
+            <li className="mt-3">Safety is the number one priority</li>
+            <li className="mt-3">Trying any wild idea and making mistakes or failures are okay but not trying anything is not okay</li>
+          </ul>
+
+          <p className="mb-7">
+          In the near future, FabLanka plans to start collaborative workshops to develop new products and services that are tailored to provide solutions for pressing issues like drinking water, energy, transportation etc. These workshops will be conducted with the help of professionals locally and internationally.
+          </p>
+
+
+        </div>
       </div>
     </Layout>
   );
