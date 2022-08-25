95418a0f2b1801d544d1df4f7ea59003fe0291b2
theekshanaj123 <106722853+theekshanaj123@users.noreply.github.com>
2022-08-25 09:42:33 +0530

    12

--- frontend/src/components/makandura.component/OurProject.js
+++ frontend/src/components/makandura.component/OurProject.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Slider from "react-slick";
 import Link from "next/link";
-import { data } from "../data.js";
+// import { data } from "../data.js";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { HomeWrapper } from "../style";
 import styles from "../pastevent.component/PastEvent.module.css";
 import { API_URL } from "../../config/index";
-import { datamaknaduraproject } from "../data_makanduraproject";
-
+import { makanduraproject } from "../data_makanduraproject";
 const PostEvent = () => {
   var settings = {
     dots: false,
@@ -52,24 +51,21 @@ const PostEvent = () => {
         <h1 className="text-center pb-3 text-3xl text-[#205DA1]">Our Project</h1>
         <HomeWrapper>
           <Slider {...settings} className={styles.card_container}>
-            {data.map((item, index) => (
+            {makanduraproject.map((item, index) => (
               <div className="p-7 sm:w-1/2 lg:w-1/3">
                 <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                   <img
                     className="lg:auto md:h-48 w-full object-cover object-center hover:scale-125 duration-300 ease-in-out"
-                    src="https://picsum.photos/id/1016/720/400"
+                    src={item.image}
                     alt="blog"
                   />
                   <Link href="/blog/[id]" as={`/blog/${item.id}`}>
                     <div className="p-6 hover:bg-[#243f5e] hover:text-white transition duration-300 ease-in">
                       <h1 className="sm:text-lg md:text-xl lg:text-2xl font-semibold mb-3">
-                        Mountains are alone
+                        {item.title}
                       </h1>
                       <p className="leading-relaxed mb-3">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Aperiam modi, expedita quos doloremque autem ipsum
-                        itaque incidunt ipsam reprehenderit fuga! Dolores
-                        quisquam eius cum accusamus?
+                        {item.summery}
                       </p>
                       <div className="flex items-center flex-wrap ">
                         {/* <h2 className="text-base text-right font-medium text-indigo-300 md:mb-2 lg:mb-0">
