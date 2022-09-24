2dad7e0f35c43f0730f358d7340c54457c0381f4
Nilupul Madhawa <dnmadawa@gmail.com>
2022-09-24 23:56:20 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,13 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ["assets.example.com"],
     loader: "akamai",
     path: "",
   },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.optimization.splitChunks.cacheGroups = {}
-    config.optimization.minimize = true;
-    return config
-  },
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
-  }
+  distDir: '.next',
 };
+
+module.exports = nextConfig
