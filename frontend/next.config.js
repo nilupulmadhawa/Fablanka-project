4610bd82ad82613a4684ec26c894e973a28d4531
Nilupul Madhawa <dnmadawa@gmail.com>
2022-09-25 00:33:33 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: ["assets.example.com"],
-    loader: "akamai",
-    path: "",
-  },
   distDir: '.next',
 };
 
